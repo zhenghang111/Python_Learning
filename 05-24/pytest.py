dd866392e663cc460b98a5e820898bfa2282af93
hzheng <zhenghang111@hotmail.com>
2018-05-24 11:44:04 -0400

    Initial commit

---
+++ 05-24/pytest.py
@@ -0,0 +1,18 @@
+import pymongo
+import base64
+print('testest  d')
+print('testest  d')
+
+
+print('test wtttldddd')
+print('test wtttldddd')
+
+a=1
+if a == 1:
+    print (a)
+
+
+def func1():
+    print('1')
+
+
